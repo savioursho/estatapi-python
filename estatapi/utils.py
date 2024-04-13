9f95d7c1e14976c1081f508fe56d2c7840e2699c
saviour <savioursho@gmail.com>
2024-04-13 23:59:18 +0000

    delete unnecessary files

--- estatapi/utils.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-def list_stats_field() -> pd.DataFrame:
-    url = "https://www.e-stat.go.jp/api/sites/default/files/uploads/2015/11/statsfield.csv"
-    df = pd.read_csv(url, encoding="shift-jis", dtype=str)
-    return df
